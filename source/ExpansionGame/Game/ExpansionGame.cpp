d95e9222287bc43b2c2d65e0949aded515f12a63
Maks <141412995+drbdedevil@users.noreply.github.com>
2025-01-24 02:50:17 +0500

    Create Vulkan Instance

--- source/ExpansionGame/Game/ExpansionGame.cpp
+++ source/ExpansionGame/Game/ExpansionGame.cpp
@@ -1,9 +1,4 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtc/random.hpp>
+#include <vulkan/vulkan.h>
 
 #include "ExpansionGame.h"
 #include <Engine/CameraManager.h>
@@ -134,65 +129,20 @@ PrimitiveShape::PrimitiveShape(int id)
 		((ID & 0x00FF0000) >> 16) / 255.0f
 	);
 
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-	glCompileShader(vertexShader);
-
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-	glCompileShader(fragmentShader);
-
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	
 }
 
 PrimitiveShape::~PrimitiveShape()
 {
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-
-	glDeleteVertexArrays(1, &VAOPlane);
-	glDeleteBuffers(1, &VBOPlane);
-
-	glDeleteProgram(shaderProgram);
+	
 }
 
 Cube::Cube(int id) : PrimitiveShape(id)
 {
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glBindVertexArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
 
 	// ----------------------------------------------------------------------------
 
-	glGenVertexArrays(1, &VAOPlane);
-	glGenBuffers(1, &VBOPlane);
-
-	glBindVertexArray(VAOPlane);
-	glBindBuffer(GL_ARRAY_BUFFER, VBOPlane);
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(TriangleVertices), TriangleVertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glBindVertexArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 Cube::~Cube()
@@ -202,71 +152,5 @@ Cube::~Cube()
 
 void Cube::Update(glm::vec3 Location)
 {
-	glEnable(GL_DEPTH_TEST);
 
-	// Отрисовка куба линиями
-	
-	glm::mat4 transform = glm::mat4(1.f);
-	CameraManager* CameraMan = CameraManager::GetCameraManager();
-
-	transform = glm::translate(transform, Location);
-	
-	// Матрица вида (положение камеры)
-	glm::mat4 view = glm::lookAt(
-		CameraMan->GetPosition(), // Позиция камеры
-		CameraMan->GetPoint(),    // Точка, на которую смотрит камера
-		CameraMan->GetDirection() // Направление "Вверх"
-	);
-
-	// Матрица проекции (перспективная проекция)
-	glm::mat4 projection = glm::perspective(
-		glm::radians(45.f), // Угол обзора
-		800.f / 600.f,      // Соотношение сторон экрана
-		0.1f,               // Ближняя плоскость отсечения
-		100.f               // Дальная плоскость отсечения
-	);
-
-	// Передаём матрицу в шейдер
-	GLint transformLoc = glGetUniformLocation(shaderProgram, "model");
-	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
-
-	GLint viewLoc = glGetUniformLocation(shaderProgram, "view");
-	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-
-	GLint projectionLoc = glGetUniformLocation(shaderProgram, "projection");
-	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
-	// Передача цвета в шейдер
-	GLint colorLoc = glGetUniformLocation(shaderProgram, "color");
-	glUniform4f(colorLoc, 0.f, 0.f, 0.f, 1.f);
-
-	glBindVertexArray(VAO);
-	glDrawArrays(GL_LINES, 0, 24);
-
-	// Отрисовка плейна треугольниками
-
-	GLint colorCell = glGetUniformLocation(shaderProgram, "color");
-	glUniform4f(colorCell, Color.x, Color.y, Color.z, 1.f);
-	// glUniform4f(colorCell, FramebufferColor.x, FramebufferColor.y, FramebufferColor.z, 1.f);
-
-	glBindVertexArray(VAOPlane);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-
-	// Рендеринг уникальных цветов
-
-	glDisable(GL_DEPTH_TEST);
-
-	glBindFramebuffer(GL_FRAMEBUFFER, CameraMan->fbo);
-	// glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
-	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
-	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
-	glUniform4f(colorCell, FramebufferColor.x, FramebufferColor.y, FramebufferColor.z, 1.f);
-
-	glUseProgram(shaderProgram);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
--- source/ExpansionGame/main.cpp
+++ source/ExpansionGame/main.cpp
@@ -1,227 +1,166 @@
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+
 #include <iostream>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtc/random.hpp>
-#include <ExpansionGame/Game/ExpansionGame.h>
-#include <Engine/CameraManager.h>
+#include <vector>
+#include <stdexcept>
+#include <cstdlib>
 
-#pragma comment (lib, "opengl32.lib")
+#define USE_DEBUG_INFO true
 
-static GLuint fbo = 0;
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
-void MouseCallback(GLFWwindow* window, int button, int action, int mods) 
+// ------------------------------
+
+static class Utils
 {
-    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) 
+public:
+    static bool checkExtensionSupport(const char** ExtensionsToCheck, uint32_t ExtensionCount, const std::vector<VkExtensionProperties>& AvailableExtenstions, const char* Object)
     {
-        double xpos, ypos;
-        glfwGetCursorPos(window, &xpos, &ypos);
-
-        // std::cout << "(" << xpos << ", " << ypos << ")" << std::endl;
-
-        glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-
-        GLubyte pixel[3];
-        glReadPixels(xpos, 600 - ypos, 1, 1, GL_RGB, GL_UNSIGNED_BYTE, pixel);
-
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-        // Конвертируем цвет в ID
-        int clickedObjectID = pixel[0] + (pixel[1] << 8) + (pixel[2] << 16);
-
-        if (clickedObjectID > 0) 
+        // Выводим количество необходимых расширений для объекта
+        std::cout << "Required " << Object << " Extension Count - " << ExtensionCount << ":" << std::endl;
+        // Выводим необходимые расширения для объекта
+        for (uint32_t i = 0; i < ExtensionCount; ++i)
         {
-            std::cout << "Объект с ID: " << clickedObjectID << " выбран." << std::endl;
+            std::cout << "\t" << ExtensionsToCheck[i] << std::endl;
+        }
 
-            if (ExpansionGame::Instance())
+        // Ищем их
+        for (uint32_t i = 0; i < ExtensionCount; ++i)
+        {
+            bool bHasRequiredExtension = false;
+            for (const auto& AvailableExtension : AvailableExtenstions)
             {
-                if (Cube* findedCube = ExpansionGame::Instance()->GetCubeByID(clickedObjectID))
+                if (std::strcmp(AvailableExtension.extensionName, ExtensionsToCheck[i]))
                 {
-                    std::cout << findedCube->GetID() << " выбран." << std::endl;
-                    findedCube->SetColor(glm::vec3(0.1f, 0.7f, 0.15f));
+                    bHasRequiredExtension = true;
+                    break;
                 }
             }
-        }
-        else 
-        {
-            std::cout << "Объект не найден - " << clickedObjectID << std::endl;
-        }
-    }
-}
 
-static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) 
+            if (!bHasRequiredExtension)
+            {
+                throw std::runtime_error("Program hasn't required extesions: " + std::string(ExtensionsToCheck[i]));
+            }
+        }
+
+        std::cout << "All required extensions are available for " << Object << "!" << std::endl;
+    }
+};
+
+// ------------------------------
+
+class Application
 {
-    if (key == GLFW_KEY_W/* && action == GLFW_PRESS*/) 
+public:
+    void run()
     {
-        std::cout << "W" << std::endl;
-        CameraManager* CameraMan = CameraManager::GetCameraManager();
-        if (CameraMan)
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    void initWindow()
+    {
+        // инициализирует GLFW библиотеку:
+        glfwInit();
+        // Так как GLFW изначально была создана для контекста OpenGL, нам необходимо сказать, чтобы этот контекст не создавался следующим вызовом:
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        // создание окна:
+        window = glfwCreateWindow(WIDTH, HEIGHT, "MaxVulkan", nullptr, nullptr);
+    }
+    void initVulkan()
+    {
+        createInstance();
+    }
+    void mainLoop()
+    {
+        // Пока не встретим исключение или не закроем окно, крутим цикл:
+        while (!glfwWindowShouldClose(window))
         {
-            CameraMan->AddCameraPosition(glm::vec3(0.f, 0.25f, 0.f));
+            // Обрабатывает события окна и ввода.
+            glfwPollEvents();
         }
     }
-    if (key == GLFW_KEY_S)
+    void cleanup()
     {
-        std::cout << "S" << std::endl;
-        CameraManager* CameraMan = CameraManager::GetCameraManager();
-        if (CameraMan)
+        vkDestroyInstance(instance, nullptr);
+
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+
+    void createInstance()
+    {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "MaxVulkan";
+        appInfo.applicationVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        // Проверка поддержки расширения:
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+#ifdef USE_DEBUG_INFO 
+    #if USE_DEBUG_INFO == true
+        std::cout << "Available Extension Count - " << extensionCount << ":" << std::endl;
+        for (const auto& extension : extensions)
         {
-            CameraMan->AddCameraPosition(glm::vec3(0.f, -0.25f, 0.f));
+            std::cout << "\t" << extension.extensionName << std::endl;
+        }
+        Utils::checkExtensionSupport(glfwExtensions, glfwExtensionCount, extensions, "GLFW");
+    #endif
+#endif
+
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        if (result != VK_SUCCESS)
+        {
+            throw std::runtime_error("Failed to create instance!");
         }
     }
-    if (key == GLFW_KEY_A)
-    {
-        std::cout << "A" << std::endl;
-        CameraManager* CameraMan = CameraManager::GetCameraManager();
-        if (CameraMan)
-        {
-            CameraMan->AddCameraPosition(glm::vec3(-0.25f, 0.f, 0.f));
-        }
-    }
-    if (key == GLFW_KEY_D)
-    {
-        std::cout << "D" << std::endl;
-        CameraManager* CameraMan = CameraManager::GetCameraManager();
-        if (CameraMan)
-        {
-            CameraMan->AddCameraPosition(glm::vec3(0.25f, 0.f, 0.f));
-        }
-    }
-}
 
-void CreateFBO()
-{
-    GLuint colorAttachment, rbo;
-
-    // Создаём FBO
-    glGenFramebuffers(1, &fbo);
-    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-
-    // Создаём текстуру для хранения цветов (идентификаторов)
-    glGenTextures(1, &colorAttachment);
-    glBindTexture(GL_TEXTURE_2D, colorAttachment);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 800, 600, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorAttachment, 0);
-
-    // Создаём буфер глубины
-    glGenRenderbuffers(1, &rbo);
-    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 800, 600);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rbo);
-
-    // Проверяем статус FBO
-    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-        std::cerr << "Ошибка: Framebuffer не завершён!" << std::endl;
-    }
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-}
+    // -------------------------------------
+    GLFWwindow* window;
+    VkInstance instance;
+};
 
 int main() 
 {
     setlocale(LC_ALL, "rus");
 
-    // Инициализация GLFW
-    if (!glfwInit()) {
-        std::cerr << "Не удалось инициализировать GLFW" << std::endl;
-        return -1;
-    }
+    Application app;
 
-    // Настройка GLFW
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
-
-    // Создание окна
-    GLFWwindow* window = glfwCreateWindow(800, 600, "MaxOpenGL", nullptr, nullptr);
-    if (!window) {
-        std::cerr << "Не удалось создать окно" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    
-    // Инициализация GLEW
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "Не удалось инициализировать GLEW" << std::endl;
-        return -1;
-    }
-    CreateFBO();
-
-    /*Cube cube(1);
-    Cube cube2(2);
-    Cube cube3(3);
-
-    Cube cube4(4);
-    Cube cube5(5);
-    Cube cube6(6);
-    Cube cube7(7);
-    Cube cube8(8);
-    Cube cube9(9);*/
-
-    ExpansionGame::CreateInstance(100, 100);
-
-    CameraManager* CameraMan = CameraManager::GetCameraManager();
-    if (CameraMan)
+    try
     {
-        CameraMan->SetPosition(glm::vec3(0.f, -5.f, 10.f));
-        CameraMan->SetPoint(glm::vec3(0.f, 0.f, 0.f));
-        CameraMan->SetDirection(glm::vec3(0.f, 0.f, 1.f));
-
-        CameraMan->fbo = fbo;
+        app.run();
     }
-
-    // Настройка области просмотра
-    glViewport(0, 0, 800, 600);
-
-    glfwSetKeyCallback(window, KeyCallback);
-    glfwSetMouseButtonCallback(window, MouseCallback);
-
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-    glLineWidth(1.6f);
-    while (!glfwWindowShouldClose(window))
+    catch (const std::exception& e)
     {
-        glClearColor(0.5f, 0.5f, 0.6f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
-        float time = glfwGetTime();
-        float zCube = sin(time);
-        float zCube2 = cos(time);
-
-        if (ExpansionGame::Instance())
-        {
-            ExpansionGame::Instance()->Update();
-        }
-
-        /*cube.Update(glm::vec3(1.05f, 0.f, 0.f));
-        cube2.Update(glm::vec3(-1.05f, 0.f, 0.f));
-        cube3.Update(glm::vec3(0.f, 0.f, 0.f));
-
-        cube4.Update(glm::vec3(1.05f, 1.05f, 0.f));
-        cube5.Update(glm::vec3(-1.05f, 1.05f, 0.f));
-        cube6.Update(glm::vec3(0.f, 1.05f, 0.f));
-        cube7.Update(glm::vec3(1.05f, -1.05f, 0.f));
-        cube8.Update(glm::vec3(-1.05f, -1.05f, 0.f));
-        cube9.Update(glm::vec3(0.f, -1.05f, 0.f));*/
-
-        if (CameraMan)
-        {
-            float offset = 5.f * (sin(time) - 1.f) - 5.f * ((sin(time) + 1.f) / 2.f);
-            // CameraMan->SetPosition(glm::vec3(0.f, offset, 0.f));
-            // std::cout << offset << std::endl;
-        }
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
-    
-    glfwTerminate();
-    CameraManager::DeleteCamera();
-    return 0;
+
+    return EXIT_SUCCESS;
 }
